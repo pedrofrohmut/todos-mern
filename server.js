f4d37e775d726493116761ee4f1c1415c4a402a0
Pedro Frohmut <pedrofrohmut@gmail.com>
2020-06-05 19:48:20 -0300

    Connect to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+
+dotenv.config({ path: "./config/config.env" })
+
+connectDB()
 
 const app = express()
 
@@ -9,6 +15,6 @@ app.use(express.json())
 // @Middleware: Routes for Todos Controller
 app.use("/api/v1/todos", require("./routes/todos"))
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server is running on port: ${PORT}`))
