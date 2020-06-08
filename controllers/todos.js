075fabd267645867e1bc319196e5bb8b403d8a7b
Pedro Frohmut <pedrofrohmut@gmail.com>
2020-06-08 14:15:16 -0300

    Update and Delete methods on TodoController

--- controllers/todos.js
+++ controllers/todos.js
@@ -12,7 +12,7 @@ exports.getTodos = async (req, res, next) => {
     console.log(err)
     return res.status(500).json({
       success: false,
-      error: "Error to get all the Todos: " + err
+      error: "Error to get all the Todos: " + err.message
     })
   }
 }
@@ -50,15 +50,51 @@ exports.addTodo = async (req, res, next) => {
     console.log(err)
     return res.status(500).json({
       success: false,
-      error: "Error to add Todo: " + err
+      error: "Error to add Todo: " + err.message
     })
   }
 }
 
 exports.deleteTodo = async (req, res, next) => {
-  res.send("DELETE TODO")
+  try {
+    const todo = await Todo.findById({ _id: req.params.id })
+    if (!todo) {
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found with the passed Id"
+      })
+    }
+    await Todo.deleteOne(todo)
+    return res.status(200).json({
+      success: true,
+      data: todo,
+      message: "Todo deleted"
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      message: "Error to delete todo: " + err.message
+    })
+  }
 }
 
 exports.updateTodo = async (req, res, next) => {
-  res.send("UPDATE TODO")
+  try {
+    const todo = await Todo.findById(req.params.id)
+    const { name, description, category } = req.body
+    const updatedTodo = { ...todo._doc, name, description, category }
+    await Todo.update(todo, { name, description, category })
+    res.status(200).json({
+      success: true,
+      data: { "before changes": todo, "Updated todo": updatedTodo },
+      message: "Todo updated"
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      message: "Error to update todo: " + err.message
+    })
+  }
 }
